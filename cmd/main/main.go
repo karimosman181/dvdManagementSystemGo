ba7717339ef0c2c1d22964beaa3ea24873a84561
Kareem Osman <kareem@contactza.eu>
2023-07-16 22:15:56 +0300

    add main func

---
+++ cmd/main/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"dvdManagementSystemGo/pkg/routes"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	routes.RegisterDVDStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
